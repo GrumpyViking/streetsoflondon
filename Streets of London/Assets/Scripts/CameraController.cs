f888b39b825caa5ed07acce845ef4804c7f8dbb4
GrumpyViking <37813025+GrumpyViking@users.noreply.github.com>
2018-04-08 17:40:20 +0200

    CleanUp & CamMovement
    Kamera Bewegung (ver. 1)
    Aufgeräumt
    Beispiel Objekte hinzugefügt

---
+++ Streets of London/Assets/Scripts/CameraController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+    public int cameraCurrentZoom = 8;
+    public int cameraZoomMax = 20;
+    public int cameraZoomMin = 5;
+    public Vector2 panLimit;
+
+    void Start()
+    {
+        Camera.main.orthographicSize = cameraCurrentZoom;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w")|| Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0) // back
+        {
+            if (cameraCurrentZoom < cameraZoomMax)
+            {
+                cameraCurrentZoom += 1;
+                Camera.main.orthographicSize = Mathf.Max(Camera.main.orthographicSize + 1);
+            }
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
+        {
+            if (cameraCurrentZoom > cameraZoomMin)
+            {
+                cameraCurrentZoom -= 1;
+                Camera.main.orthographicSize = Mathf.Min(Camera.main.orthographicSize - 1);
+            }
+        }
+        
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+
+
+        transform.position = pos;
+	}
+}
