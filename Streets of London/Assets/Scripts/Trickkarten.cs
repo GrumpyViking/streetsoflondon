ccba9f594fa7db715010d6d18037afa7334556de
HTFritzsch <gubbel97@web.de>
2018-04-18 16:42:24 +0200

    Trickkartenmenue und Anpassung an Bildschirm
    1 Scriptable Object als Trickkarte
    UI passt sich an screan an
    Trickkartenmenu

---
+++ Streets of London/Assets/Scripts/Trickkarten.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="neue Trickkarte", menuName ="Trickkarte" )]
+public class Trickkarten : ScriptableObject {
+
+    public string trickkartenname;
+    public string wirung;
+    public int preis;
+
+    public Sprite wirkungsbereich;
+    public Sprite trickkartenbild;
+
+
+	}
+
