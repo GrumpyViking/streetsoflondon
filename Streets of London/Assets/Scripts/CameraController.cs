7e947455a218168a606c31385e139495e8029903
GrumpyViking <37813025+GrumpyViking@users.noreply.github.com>
2018-04-29 12:00:32 +0200

    Alpha Scene

--- Streets of London/Assets/Scripts/CameraController.cs
+++ Streets of London/Assets/Scripts/CameraController.cs
@@ -6,16 +6,19 @@ public class CameraController : MonoBehaviour {
 
     public float panSpeed = 20f;
     public float panBorderThickness = 10f;
-    public float cameraCurrentZoom = 50f;
-    public float cameraZoomMax = 50f;
-    public float cameraZoomMin = 10f;
     public Vector2 panLimit;
-    public bool cameraActiv = true;
 
-    void Start()
-    {
-        Camera.main.orthographicSize = cameraCurrentZoom;
-    }
+    public bool cameraActiv = true;
+    public Camera camera;
+
+    public float maxZoom = 90f;
+    public float zoomSpeed = 20f;
+    public float minZoom = 20f;
+    public float currentZoom = 65f;
+
+    float scroll;
+
+
 
     //
     void LateUpdate () {
@@ -39,22 +42,23 @@ public class CameraController : MonoBehaviour {
             {
                 pos.x -= panSpeed * Time.deltaTime;
             }
-            if (Input.GetAxis("Mouse ScrollWheel") < 0) // back
+            if(Input.GetAxis("Mouse ScrollWheel") < 0 && currentZoom <= maxZoom)   //Heraus Zoomen
             {
-                if (cameraCurrentZoom < cameraZoomMax)
-                {
-                    cameraCurrentZoom += 1;
-                    Camera.main.orthographicSize = Mathf.Max(Camera.main.orthographicSize + 1);
-                }
+                scroll = Input.GetAxisRaw("Mouse ScrollWheel");
+                
+                camera.fieldOfView -= scroll * zoomSpeed;
+                currentZoom = camera.fieldOfView;
             }
-            if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
+            if (Input.GetAxis("Mouse ScrollWheel") > 0 && currentZoom >= minZoom  )
             {
-                if (cameraCurrentZoom > cameraZoomMin)
-                {
-                    cameraCurrentZoom -= 1;
-                    Camera.main.orthographicSize = Mathf.Min(Camera.main.orthographicSize - 1);
-                }
+                scroll = Input.GetAxisRaw("Mouse ScrollWheel");
+                Debug.Log(Input.GetAxisRaw("Mouse ScrollWheel"));
+                camera.fieldOfView -= scroll * zoomSpeed;
+                currentZoom = camera.fieldOfView;
+                Debug.Log(currentZoom);
             }
+
+            
 
             pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
             pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
