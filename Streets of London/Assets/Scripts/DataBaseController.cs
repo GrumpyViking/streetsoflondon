72ce62326fe5d5a84c962d8d6043035cdf917f0f
GrumpyViking <37813025+GrumpyViking@users.noreply.github.com>
2018-05-06 16:53:55 +0200

    Datenbank script

---
+++ Streets of London/Assets/Scripts/DataBaseController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+
+public class DataBaseController : MonoBehaviour {
+
+    IDbConnection dbconn;
+    IDataReader reader;
+    IDbCommand dbcmd;
+
+    void OpenDBConnection()
+    {
+        string conn = "URI=file:" + Application.dataPath + "/DB/PlayerData.db"; //Path to database.
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+    }
+
+
+    void Start () {
+        OpenDBConnection();
+        dbcmd = dbconn.CreateCommand();
+        dbcmd.CommandText = "DELETE FROM Spieler";
+        dbcmd.ExecuteNonQuery();
+
+        dbcmd.CommandText = "VACUUM";
+        dbcmd.ExecuteNonQuery();
+        CloseDBConnection();
+    }
+
+    public string RequestFromDB(string query)
+    {
+        OpenDBConnection();
+        dbcmd = dbconn.CreateCommand();
+        dbcmd.CommandText = query;
+        reader = dbcmd.ExecuteReader();
+            int id = reader.GetInt32(0);
+            string name = reader.GetString(1);
+            int gold = reader.GetInt32(2);
+        reader.Close();
+        reader = null;
+        CloseDBConnection();
+
+        return "id " + id + "  name =" + name + "  gold =" + gold;
+    }
+
+    public void WriteToDB(string query)
+    {
+        OpenDBConnection();
+        dbcmd = dbconn.CreateCommand();
+        dbcmd.CommandText = query;
+        dbcmd.ExecuteNonQuery();
+        CloseDBConnection();
+    }
+
+    void CloseDBConnection()
+    {
+        
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+    }
+	
+	
+}
--- Streets of London/Assets/Scripts/MainMenuManager.cs
+++ Streets of London/Assets/Scripts/MainMenuManager.cs
@@ -12,10 +12,11 @@ public class MainMenuManager : MonoBehaviour {
     Text pot;
     public GameObject playerTwoText;
     Text ptt;
+    
     public GameObject ChooseStartingPlayer;
     Text ctp;
     private SwitchScene switchSceneScript;
-
+    public DataBaseController dbc;
 
     public void StartGame()
     {
@@ -24,48 +25,11 @@ public class MainMenuManager : MonoBehaviour {
 
         ctp = ChooseStartingPlayer.GetComponent<Text>();
 
-        #region Datenbank
-        //setup database
-        string conn = "URI=file:" + Application.dataPath + "/DB/PlayerData.db"; //Path to database.
-        IDbConnection dbconn;
-        dbconn = (IDbConnection)new SqliteConnection(conn);
-        dbconn.Open(); //Open connection to the database.
-        IDbCommand dbcmd = dbconn.CreateCommand();
-
-        dbcmd.CommandText = "DELETE FROM Spieler";
-        dbcmd.ExecuteNonQuery();
-
-        dbcmd.CommandText = "VACUUM";
-        dbcmd.ExecuteNonQuery();
-
-
-        dbcmd.CommandText = "INSERT INTO Spieler(Name, Gold) VALUES ('" + pot.text + "',20)";
-        dbcmd.ExecuteNonQuery();
-
-        dbcmd.CommandText = "INSERT INTO Spieler(NAME, GOLD) Values('" + ptt.text + "' ,'20')";
-        dbcmd.ExecuteNonQuery();
-
-        string sqlQuery = "SELECT * FROM Spieler";
-        dbcmd.CommandText = sqlQuery;
-        IDataReader reader = dbcmd.ExecuteReader();
-        while (reader.Read())
-        {
-            int id = reader.GetInt32(0);
-            string name = reader.GetString(1);
-            int gold = reader.GetInt32(2);
-
-            Debug.Log("id= " + id + "  name =" + name + "  gold =" + gold);
-        }
-        reader.Close();
-        reader = null;
-        dbcmd.Dispose();
-        dbcmd = null;
-        dbconn.Close();
-        dbconn = null;
-        #endregion
+        
+        dbc.WriteToDB("INSERT INTO Spieler(Name, Gold) VALUES ('" + pot.text + "','20')");
+        dbc.WriteToDB("INSERT INTO Spieler(Name, Gold) VALUES('" + ptt.text + "' ,'20')");
 
         PassthrougData.startPlayer = StartingPlayer(ctp.text);
-
         PassthrougData.player1 = pot.text;
         PassthrougData.player2 = ptt.text;
 
