21ccde7b78ddbd2de79ff24354da5ea5ac56e78d
Fabsus98 <39088748+Fabsus98@users.noreply.github.com>
2018-05-13 23:37:41 +0200

    Kleine KaufMenuScript Änderung

---
+++ Streets of London/Assets/Scripts/KaufMenuScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KaufMenuScript : MonoBehaviour
+{
+    public GameObject kaufMenuScriptObject;
+    public GameObject anzeige1;
+
+    public void OeffneKaufmenue()
+    {
+        kaufMenuScriptObject.SetActive(true);
+    }
+
+    public void SchließeKaufmenu()
+    {
+        kaufMenuScriptObject.SetActive(false);
+    }
+
+    public void ErhoeheAnzeige1()
+    {
+        /*
+        int x = Convert.ToInt32();
+        String text1 = Convert.ToString(x++);
+        anzeige1.GetComponent<UnityEngine.UI.Text>().text = text1;
+        */
+    }
+}
+
