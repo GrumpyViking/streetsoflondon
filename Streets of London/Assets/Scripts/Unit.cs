23fb1c7698a11ec5f61a9a7fdac103209fccfdd8
GrumpyViking <37813025+GrumpyViking@users.noreply.github.com>
2018-05-29 21:10:26 +0200

    fix

---
+++ Streets of London/Assets/Scripts/Unit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+    private int id;
+    private string name;
+    private int actionValue;
+    private int attValue;
+    private int lifeValue;
+    private int defValue;
+    private int reach;
+    private int cost;
+
+    public Unit(int id, string name, int actionValue, int attValue, int lifeValue, int defValue, int reach, int cost)
+    {
+        this.id = id;
+        this.name = name;
+        this.actionValue = actionValue;
+        this.attValue = attValue;
+        this.lifeValue = lifeValue;
+        this.defValue = defValue;
+        this.reach = reach;
+        this.cost = cost;
+    }
+
+    
+
+
+}
